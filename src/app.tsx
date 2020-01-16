9a0f93b3b1d26dd34c5980954b8204e94fcd8e00
Nicolas <elendil_le_grand@hotmail.com>
2020-01-16 19:44:13 +0100

    ajout main à html

--- src/app.tsx
+++ src/app.tsx
@@ -2,5 +2,9 @@ import { JSX } from 'wapitis'
 import TodoList from './components/todo-list'
 import './www/styles/main.css'
 
-document.body.appendChild(<TodoList></TodoList>)
+document.body.appendChild(
+    <main>
+        <TodoList></TodoList>
+    </main>
+)
 document.body.appendChild(<footer><div>Créé par Nicolas Boyer avec <a href='https://github.com/NicolasBoyer/wapitis'>Wapitis</a>.</div><div>© <strong>WAPITIS</strong>. Tous droits réservés.</div></footer>)
